f88b6482d859c0ab2aac23af78f4122e604ee0dc
Emma Chory <emmachory@gmail.com>
2021-11-02 14:25:59 -0600

    Build activity class

---
+++ Activity.js
@@ -0,0 +1,19 @@
+class Activity {
+  constructor(category, description, minutes, seconds, id) {
+    this.category = category;
+    this.description = description;
+    this.minutes = minutes;
+    this.seconds = seconds;
+    this.completed = false;
+    this.id = id;
+  }
+  countdown() {
+    //begin a timer that counts down by seconds from input time
+  }
+  markComplete() {
+    // when timer is done, change time display to say complete
+  }
+  saveToStorage() {
+    //when we click log activity, save to local storage 
+  }
+}
