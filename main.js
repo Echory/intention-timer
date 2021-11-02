dede29559bc830bc31f86419840c802b7451cd49
Emma Chory <emmachory@gmail.com>
2021-11-02 16:20:07 -0600

    Create variables, event listeners and functions to change button colors

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
 // When button is clicked, both icon and outline change color to indicate they have been selected
+
+//VARIABLES//
 var studyBtn = document.querySelector('.study');
+var meditateBtn = document.querySelector('.meditate');
+var exerciseBtn = document.querySelector('.exercise');
 var studyPlain = document.querySelector('.study-plain');
 var studyActive = document.querySelector('.study-active');
+var meditatePlain = document.querySelector('.meditate-plain');
+var meditateActive = document.querySelector('.meditate-active');
+var exercisePlain = document.querySelector('.exercise-plain');
+var exerciseActive = document.querySelector('.exercise-active');
 
-studyBtn.addEventListener('click', activateColor);
+//EVENT LISTENERS//
+studyBtn.addEventListener('click', activateColorStudy);
+meditateBtn.addEventListener('click', activateColorMeditate);
+exerciseBtn.addEventListener('click', activateColorExercise);
 
-function activateColor() {
+//FUNCTIONS//
+function activateColorStudy() {
   event.preventDefault();
-  studyPlain.classList.add('hidden');
-  studyActive.classList.remove('hidden');
-}
+    studyPlain.classList.add('hidden');
+    studyActive.classList.remove('hidden');
+  }
+
+  function activateColorMeditate() {
+    event.preventDefault();
+    meditatePlain.classList.add('hidden');
+    meditateActive.classList.remove('hidden');
+  }
+
+  function activateColorExercise() {
+    event.preventDefault();
+    exercisePlain.classList.add('hidden');
+    exerciseActive.classList.remove('hidden');
+  }
+
